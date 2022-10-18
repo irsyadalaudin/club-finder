e7fccdf911d7eee842197395f0c28dc534631b10
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2022-10-18 14:57:34 +0200

    nambahin club-item.js

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -1,18 +1,17 @@
-// class ClubItem extends HTMLElement {
-//     set club(clubs) {
-//         this.clubs = clubs;
-//         this.render();
-//     }
+class ClubItem extends HTMLElements {
+    set club(club) {
+        this._club;
+        this.render()
+    }
 
-//     render() {
-//         this.innerHTML -= `
-//             <img class="fan-art-club" src="${this.club.fanArt}">
-//             <div class="club-info">
-//                 <h2>${this.club.name}</h2>
-//                 <p>${this._club.description}</p>
-//             </div>
-//         `;
-//     }
-// }
+    render() {`
+        <img class="fan-art-club" src= "${fanArt}" alt="Fan Art">
+        <div class="club-info">
+            <h2>${name}</h2>
+            <p>${description}</p>
+        </div>
+    `;
+    }
+}
 
-// customElements.define("club-item", ClubItem)
+customElements.define('club-item', ClubItem);
