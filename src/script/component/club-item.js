3658d3ba82827082e35d6e79b9f056e888f7c0bd
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2022-10-18 19:30:17 +0200

    ngebernerin club-item.js,  ngebenerin setter/gette this._club = club; di club-item.js dan this._clubs = clubs; di club-item.js,  ngubah seluruh selector #clubList menjadi club-list dan selector .club menjadi club-item di clublist.css,  ngebenerin const clubItemElement = document.createElement('club-item') sebelumnya ada typo pada document.createElementElement

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -1,16 +1,17 @@
-class ClubItem extends HTMLElements {
+class ClubItem extends HTMLElement {
     set club(club) {
-        this._club;
+        this._club = club;
         this.render()
     }
 
-    render() {`
-        <img class="fan-art-club" src= "${fanArt}" alt="Fan Art">
-        <div class="club-info">
-            <h2>${name}</h2>
-            <p>${description}</p>
-        </div>
-    `;
+    render() {
+        this.innerHTML = `
+            <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
+            <div class="club-info">
+                <h2>${this._club.name}</h2>
+                <p>${this._club.description}</p>
+            </div>
+        `;
     }
 }
 
--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -1,14 +1,14 @@
 import './club-item.js';                                                       // (INI TERAKHIR) Oh ya! Dikarenakan pada berkas ini kita menggunakan custom elemen <club-item> (kita akan membuat custom elemen ini nanti pada club-item.js), maka kita perlu melakukan impor custom element dari berkas club-item.js ke berkas club-list.js ini
-class ClubList extends HTMLElements {                                          // Langkah pertama kita buat custom element <club-list> terlebih dahulu. Pada berkas club-list.js, kita buat class ClubList dengan mewarisi sifat HTMLElement.
+class ClubList extends HTMLElement {                                          // Langkah pertama kita buat custom element <club-list> terlebih dahulu. Pada berkas club-list.js, kita buat class ClubList dengan mewarisi sifat HTMLElement.
     set clubs(clubs) {                                                         // Kemudian kita buat 2 (dua) fungsi di dalamnya yaitu setter clubs, dan render (DI KOLOM 7)
-        this._clubs;
+        this._clubs = clubs;
         this.render();
     }
     
     render() {                                                                 // Kemudian kita buat fungsi render.
         this.inner = '';
         this._clubs.forEach(club => {                                          // kita lakukan proses perulangan menggunakan fungsi forEach pada properti this._clubs (DI KOLOM 3)
-            const clubItemElement = document.createElementement('club-item')   // Pada setiap iterasinya (perulangannya), kita akan mendapatkan individual club dan kita buat custom element <club-item> secara langsung. // Setiap elemen <club-item> yang terbuat akan menjadi child element dari element <club-list> ini.  // <club-item> MENJADI <li> YAITU, CHILD ELEMENT DARI <club-list> <ul>
+            const clubItemElement = document.createElement('club-item')   // Pada setiap iterasinya (perulangannya), kita akan mendapatkan individual club dan kita buat custom element <club-item> secara langsung. // Setiap elemen <club-item> yang terbuat akan menjadi child element dari element <club-list> ini.  // <club-item> MENJADI <li> YAITU, CHILD ELEMENT DARI <club-list> <ul>
             clubItemElement.club = club;
             this.appendChild(clubItemElement);
         });
