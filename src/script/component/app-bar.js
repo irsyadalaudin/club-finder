282ee6fdbeb540f224c7ee7f7628637a267439c5
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2022-10-23 10:33:11 +0200

    ngebenerin css serta meng-comment nya,   meng-cut css dan meng-paste nya di app-bar.js di dalam fungsi render() pada nilai this.shadowDOM.innerHTML

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,16 +1,16 @@
 /* (II) 12. SOLUSI: MEMBUAT app-bar COMPONENT
-class AppBar extends HTMLElement {                    // kita buat class dengan nama AppBar yang mewarisi sifat HTMLElement.
-    connectedCallback() {                             // Kemudian di dalam body block classnya, kita implementasi method connectedCallback dan membuat fungsi render. (DI KOLOM 7)
-        this.render();                                // Seperti yang sudah kita ketahui, connectedCallback() akan terpanggil ketika element telah diterapkan pada DOM. // Jika kita ingin element ini ketika diterapkan langsung melakukan rendering maka kita dapat memanggil fungsi this.render() di dalam connectedCallback.
+class AppBar extends HTMLElement {                         // kita buat class dengan nama AppBar yang mewarisi sifat HTMLElement.
+    connectedCallback() {                                  // Kemudian di dalam body block classnya, kita implementasi method connectedCallback dan membuat fungsi render. (DI KOLOM 7)
+        this.render();                                     // Seperti yang sudah kita ketahui, connectedCallback() akan terpanggil ketika element telah diterapkan pada DOM. // Jika kita ingin element ini ketika diterapkan langsung melakukan rendering maka kita dapat memanggil fungsi this.render() di dalam connectedCallback.
     
     }
 
-    render() {                                        // Kemudian di dalam body block classnya, kita buat fungsi render.
-        this.innerHTML = `<h2>Club Finder</h2>`;      // Di dalam elemen <header> di index.html terdapat elemen <div> yang menerapkan class “app-bar”. Nah kita copy element di dalam app-bar, dan paste untuk dijadikan nilai pada this.innerHTML di fungsi render().
+    render() {                                             // Kemudian di dalam body block classnya, kita buat fungsi render.
+        this.innerHTML = `<h2>Club Finder</h2>`;           // Di dalam elemen <header> di index.html terdapat elemen <div> yang menerapkan class “app-bar”. Nah kita copy element di dalam app-bar, dan paste untuk dijadikan nilai pada this.innerHTML di fungsi render().
     }
 }
 
-customElements.define('app-bar', AppBar);             // Mendefinisikan custom element yang kita buat agar dapat digunakan pada DOM
+customElements.define('app-bar', AppBar);                  // Mendefinisikan custom element yang kita buat agar dapat digunakan pada DOM
 */
 
 
@@ -26,9 +26,21 @@ class AppBar extends HTMLElement {
         this.render();
     }
                                                            // Karena kita sudah menerapkan Shadow DOM pada AppBar,
-    render() {                                             // jangan lupa pada fungsi render(),
-        this.shadowDOM.innerHTML = `<h2>Club Finder</h2>`; // kita ubah 'this.innerHTML' menjadi 'this.shadowDOM.innerHTML'
-    }
-}
+    render() {                                             // jangan lupa pada fungsi render(),  // kita ubah 'this.innerHTML' menjadi 'this.shadowDOM.innerHTML' (DI KOLOM 30)
+        this.shadowDOM.innerHTML = `
+            <style>
+                app-bar {
+                    display: block;
+                    padding: 16px;
+                    width: 100%;
+                    background-color: cornflowerblue;
+                    color: white;
+                    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                }
+            </style>
+
+            <h2>Club Finder</h2>`;                              // Kemudian buka berkas style -> appbar.css dan pindahkan (cut) seluruh kode yang ada pada berkas tersebut.
+    }                                                           // Lalu, tempel (paste) pada nilai this.shadowDOM.innerHTML dengan dibungkus oleh element <style> tepat sebelum element <h2> pada fungsi render() di berkas app-bar.js
+}                                                               // (DI KOLOM 31-40)
 
 customElements.define('app-bar', AppBar);
