427a124ad237ea1761688bc5e6469c40a179e3ac
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2022-10-23 09:29:06 +0200

    menambahkan constructor() untuk class AppBar,  melampirkan shadoow root pada shadow host, dan mengatur shadow dengan nilai open

--- src/script/component/app-bar.js
+++ src/script/component/app-bar.js
@@ -1,3 +1,4 @@
+/* (II) 12. SOLUSI: MEMBUAT app-bar COMPONENT
 class AppBar extends HTMLElement {                    // kita buat class dengan nama AppBar yang mewarisi sifat HTMLElement.
     connectedCallback() {                             // Kemudian di dalam body block classnya, kita implementasi method connectedCallback dan membuat fungsi render. (DI KOLOM 7)
         this.render();                                // Seperti yang sudah kita ketahui, connectedCallback() akan terpanggil ketika element telah diterapkan pada DOM. // Jika kita ingin element ini ketika diterapkan langsung melakukan rendering maka kita dapat memanggil fungsi this.render() di dalam connectedCallback.
@@ -11,3 +12,26 @@ class AppBar extends HTMLElement {                    // kita buat class dengan 
 }
 
 customElements.define('app-bar', AppBar);             // Mendefinisikan custom element yang kita buat agar dapat digunakan pada DOM
+*/
+
+
+/* (II) 19. SOLUSI: MENETAPKAN SHADOW DOM PADA PROYEK CLUB FINDER */
+/* */
+class AppBar extends HTMLElement {
+    constructor() {                                        // buat constructor dari class tersebut dan di dalamnya kita tetapkan shadow root
+    super();
+    this.shadowDOM = this.attachShadow({mode: 'open'});    // MELAMPIRKAN SHADOW ROOT PADA SHADOW HOST
+    }                                                      // MENGATUR MODE SHADOW DENGAN NILAI OPEN
+                                                           // SEBENARNYA TERDAPAT 2 OPSI NILAI YANG DAPAT  DIGUNAKAN DALAM PROPRERTI MODE, YAITU "OPEN" DAN "CLOSED"
+    connectedCallback() {
+        this.render();
+    
+    }
+
+    render() {
+        this.innerHTML = `<h2>Club Finder</h2>`;
+    
+    }
+}
+
+customElements.define('app-bar', AppBar);
