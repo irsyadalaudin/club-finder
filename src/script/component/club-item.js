049f7d181df33f29b7ca2cbfff992a0278e565fe
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2022-10-24 09:50:17 +0200

    menambahkan penjelasan club-item.js;   dan menambahkan judul comment di club-item.js

--- src/script/component/club-item.js
+++ src/script/component/club-item.js
@@ -1,10 +1,11 @@
-class ClubItem extends HTMLElement {
-    set club(club) {
-        this._club = club;
+/* (II) 14. SOLUSI: MEMBUAT club-item COMPONENT */
+class ClubItem extends HTMLElement {                                                // Pada berkas club-item.js, kita buat class ClubItem dengan mewarisi sifat HTMLElement.
+    set club(club) {                                                                // Kemudian kita buat 2 (dua) fungsi di dalamnya yaitu setter club (DI KOLOM 3), dan render (DI KOLOM 8)    // Kemudian kita deklarasikan fungsi setter club dan fungsi render (DI KOLOM 4, 5).
+        this._club = club;                                                          // Fungsi setter club berfungsi untuk menetapkan nilai club ke properti this._club    // yang nantinya akan digunakan pada fungsi render untuk menampilkan data individual club berdasarkan hasil pencarian.    // jadi, kita sesuaikan kode di dalam fungsi setter club menjadi seperti ini: (DI KOLOM 4, 5)                
         this.render()
     }
-
-    render() {
+                                                                                    // Lalu lakukan copy template html yang berada pada fungsi renderResult di berkas src -> script -> view -> main.js.
+    render() {                                                                      // Kemudian, lakukan paste template html untuk dilakukan assignment pada properti this.innerHTML di dalam fungsi render(). (DI KOLOM 10-15)    // Lalu, kita sesuaikan kembali properti-properti yang digunakan pada html template menjadi seperti ini:
         this.innerHTML = `
             <img class="fan-art-club" src="${this._club.fanArt}" alt="Fan Art">
             <div class="club-info">
@@ -14,5 +15,5 @@ class ClubItem extends HTMLElement {
         `;
     }
 }
-
-customElements.define('club-item', ClubItem);
+                                                                                    // // Karena pada properti this._club-lah properti atau data dari objek club disimpan.
+customElements.define('club-item', ClubItem);                                       // Kemudian pada akhir berkas club-item.js jangan lupa untuk definisikan custom element yang kita buat agar dapat digunakan pada DOM. (DI KOLOM 19)
