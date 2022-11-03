7a6baf32023f24ee5367b1352a75014c183a79b6
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2022-11-03 20:40:30 +0200

    meng-import  berkas CSS dan menyeesuaikan kembali lokasi impor dari app-bar.js dan main.js pada entry point yaitu app.js.

--- src/app.js
+++ src/app.js
@@ -10,8 +10,17 @@ document.addEventListener('DOMContentLoaded', main);
 */
 
 
-/* (II) 12. SOLUSI: MEMBUAT app-bar COMPONENT */
+/* (II) 12. SOLUSI: MEMBUAT app-bar COMPONENT
 import './script/component/app-bar.js'               // , supaya kode pada berkas app-bar.js tereksekusi, maka berkas tersebut di-impor pada berkas ini di app.js seperti berikut ini.
+import main from './script/view/main.js';
+
+document.addEventListener('DOMContentLoaded', main);
+*/
+
+
+/* (IV) 16. SOLUSI: MENERAPKAN WEBPACK PADA CLUB FINDER */
+import './styles/style.css';
+import './script/component/app-bar.js';
 import main from './script/view/main.js';
 
 document.addEventListener('DOMContentLoaded', main);
